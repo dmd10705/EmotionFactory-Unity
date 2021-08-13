d0f495d1129b627748faa161d9ff46464c2cfbc1
Emrys0118 <71679826+Emrys0118@users.noreply.github.com>
2021-08-13 13:02:40 +0800

    上傳

--- EmotionFactory/Assets/Script/Sound.cs
+++ EmotionFactory/Assets/Script/Sound.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Sound : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float R = 3f;
+    private float R2;
+    public float pitch; //寫改成的pitch值
     void Start()
     {
-        
+        R2 = R;
+        //------------
+        //取得背景音樂
+        //AudioSource source = GameObject.FindGameObjectWithTag("電視雜訊").GetComponent<AudioSource>();
+        //source.Stop();  //暫停音樂
+        //source.pitch = pitch;  //改變pitch
+        //source.Play();  //開始音樂
+        //------------
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        R2 = R2 - Time.deltaTime;
+        if (R2 <= 0)
+        {
+            //取得背景音樂
+            AudioSource source = GameObject.FindGameObjectWithTag("電視雜訊").GetComponent<AudioSource>();
+            source.Stop();  //暫停音樂
+            source.pitch = pitch;  //改變pitch
+            source.Play();  //開始音樂
+            R2 = R;
+            Debug.Log("當前frame與前一個frame的時間差為：" + Time.deltaTime + "秒");
+        }
     }
+    //--------------
+    
+
+   
+    //--------------
 }
